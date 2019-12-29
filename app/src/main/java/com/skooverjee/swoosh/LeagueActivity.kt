040d3e641ae13b94f837483b9fd2c6305a48c307
Shaheel Kooverjee <skooverjee@gmail.com>
2019-12-29 16:28:46 +0200

    Add button selection logic on League screen

--- app/src/main/java/com/skooverjee/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/skooverjee/swoosh/LeagueActivity.kt
@@ -4,17 +4,48 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun leagueNextClick(view: View) {
-        val skillIntent = Intent(this, SkillActivity::class.java)
-        startActivity(skillIntent)
+    fun mensClicked(view: View) {
+        womenButton.isChecked = false
+        coEdButton.isChecked = false
+
+        selectedLeague = if (menButton.isChecked) { MEN } else { "" }
+    }
+
+    fun womensClicked(view: View) {
+        coEdButton.isChecked = false
+        menButton.isChecked = false
+
+        selectedLeague = if (womenButton.isChecked) { WOMEN } else { "" }
+    }
+
+    fun coEdClicked(view: View) {
+        menButton.isChecked = false
+        womenButton.isChecked = false
+
+        selectedLeague = if (coEdButton.isChecked) { COED } else { "" }
+    }
+
+    fun leagueNextClicked(view: View) {
+        if (selectedLeague != "") {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+
+            startActivity(skillIntent)
+        } else {
+            Toast.makeText(this, "Select a league to continue.", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
--- app/src/main/java/com/skooverjee/swoosh/SkillActivity.kt
+++ app/src/main/java/com/skooverjee/swoosh/SkillActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 
 class SkillActivity : AppCompatActivity() {
 
+    var league = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        league = intent.getStringExtra(EXTRA_LEAGUE)
     }
 }
